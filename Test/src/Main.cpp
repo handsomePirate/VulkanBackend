b10d45ac06c91cf74aaedcd0825fdc1673b8f69e
Vojtěch Kužel <adalbert.kuzel@seznam.cz>
2021-10-02 19:28:09 +0200

    Yaml test working

--- Test/src/Main.cpp
+++ Test/src/Main.cpp
@@ -1,8 +1,24 @@
 #include <yaml-cpp/yaml.h>
+#include <Filesystem/Filesystem.hpp>
+#include <iostream>
 
 int main(int argc, char* argv[])
 {
-	YAML::LoadFile("testfile.yml");
+	Core::Filesystem filesystem(argv[0]);
+	std::string pathToYamlFile = filesystem.GetAbsolutePath("../../testfile.yml");
+	YAML::Node data = YAML::LoadFile(pathToYamlFile);
+
+	if (!data["Employees"])
+	{
+		std::cout << "error" << std::endl;
+	}
+
+	auto employees = data["Employees"];
+	
+	for (int i = 0; i < employees.size(); ++i)
+	{
+		std::cout << employees[i]["name"]  << " is good at " << employees[i]["skills"][0] << std::endl;
+	}
 
 	return 0;
 }
