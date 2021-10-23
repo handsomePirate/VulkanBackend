42c66e60f73cbada17ecc704be1846cf019f01d5
Vojtěch Kužel <adalbert.kuzel@seznam.cz>
2021-10-23 20:19:57 +0200

    Allowed logger to be overriden

--- include/VulkanBackend/Logger.hpp
+++ include/VulkanBackend/Logger.hpp
@@ -1,5 +1,10 @@
 #pragma once
+
+#ifndef VulkanLogger
+
 #include <SoftwareCore/Singleton.hpp>
 #include <SoftwareCore/Logger.hpp>
 
 #define VulkanLogger ::Core::Singleton<::Core::Logger>::GetInstance()
+
+#endif
